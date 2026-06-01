6db52851ae5bbf542416e3c0e69596cb41e613fd
farhanfadila1717 <farhan.fadila1717@gmail.com>
2026-06-01 17:00:10 +0700

    feat: add GitHub Actions workflow for deploying to GitHub Pages and update favicon

--- doc/astro.config.mjs
+++ doc/astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://farhanfadila1717.github.io',
+  base: '/easy_icons',
   vite: {
     plugins: [tailwindcss()]
   }
