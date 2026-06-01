2eec789e975ea5366bb2c195162f9fa702eb1cfd
farhanfadila1717 <farhan.fadila1717@gmail.com>
2026-06-01 17:11:18 +0700

    feat: update base path in Astro configuration

--- doc/astro.config.mjs
+++ doc/astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://farhan.is-a.dev',
-  base: '/easy_icons',
+  base: '/easy_icons/doc',
   vite: {
     plugins: [tailwindcss()]
   }
