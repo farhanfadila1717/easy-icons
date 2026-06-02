cc40a0fb1e4206b3827205fca0b21ab558540b02
farhanfadila1717 <farhan.fadila1717@gmail.com>
2026-06-02 12:07:06 +0700

    feat: add icon dialog

--- doc/src/core/utils.ts
+++ doc/src/core/utils.ts
@@ -1,3 +1,10 @@
 export function iconChar(codepoint: number): string {
   return String.fromCodePoint(codepoint);
 }
+
+export function camelCase(str: string): string {
+  return str
+    .trim()
+    .replace(/[-_\s]+(.)?/g, (_, c) => (c ? c.toUpperCase() : ""))
+    .replace(/^(.)/, (m) => m.toLowerCase());
+}
